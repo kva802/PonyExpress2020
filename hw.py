c099e556127348fd7d1149053e8570ff6d314c6b
kva802 <71887839+kva802@users.noreply.github.com>
2020-10-02 18:34:52 +0300

    Update hw.py

--- hw.py
+++ hw.py
@@ -1,14 +1,21 @@
-class rectangle:
-    def __init__(self, list_of_dots []):
-        self.dots = list_of_dots
-    def square(self):
-        if self.list_of_dots[1][1] =! self.list_of_dots[2][1]:
-            a = self.list_of_dots[1][1] - self.list_of_dots[2][1]
-        elif self.list_of_dots[1][1] =! self.list_of_dots[3][1]:
-            a = self.list_of_dots[1][1] - self.list_of_dots[3][1]
-        else:
-            a = self.list_of_dots[1][1] - self.list_of_dots[4][1]
+assert 'Пегас' == driver.title
 
-        return(a*a)
-    def perimeter(self):
-        return(a*4)
+element_login = driver.find_element_by_name("login")
+element_password = driver.find_element_by_name("password")
+enter_button = driver.find_element_by_class_name("css-1hnkt5t")
+
+element_login.send_keys('ext.mgu_education')
+element_password.send_keys('rg#P5hZm4F')
+enter_button.click()
+
+time.sleep(5)
+
+elem = driver.find_element_by_xpath("/html/body/div[1]/section/section[2]/section")
+elem.text == 'Главная страница'
+
+enter_button = driver.find_element_by_xpath("/html/body/div[1]/section/section[1]/div/div/div[2]/div/button[2]/span")
+#class="bp3-button bp3-minimal"
+enter_button.click()
+
+
+#driver.close()
