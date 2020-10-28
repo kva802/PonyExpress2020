910f16084e84dbf29933cba18d10a0ab9c183faa
kva802 <71887839+kva802@users.noreply.github.com>
2020-10-28 17:31:13 +0300

    Create test_case10.py

---
+++ test_case10.py
@@ -0,0 +1,15 @@
+import mod_init
+
+
+def test_10():
+    driver = mod_init.pony_driver_init()
+    mod_init.test_enter(driver)
+    mod_init.menu_button_click(driver)
+    mod_init.proizvodstvo_button_click(driver)
+    mod_init.regist_of_events_button_click(driver)
+    mod_init.pnsbs_71_button_click(driver)
+    mod_init.continue_without_courier(driver)
+    mod_init.nomer_object(driver)
+
+if __name__ == "__main__":
+    test_10()
