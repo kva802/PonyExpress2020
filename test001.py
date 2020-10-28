6d866935b949f4f5758cd0f45b047de89e532f2c
kva802 <71887839+kva802@users.noreply.github.com>
2020-10-28 17:16:00 +0300

    Update test001.py

--- test001.py
+++ test001.py
@@ -1,12 +1,6 @@
-import time
 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
-import init
+import mod_init
 
 #driver = init.pony_driver_init()
 #init.test_enter(driver)
@@ -15,15 +9,15 @@ import init
 #init.test_deletegroup(driver)
 #init.test_exit(driver)
 
-def test_8():
-    driver = init.pony_driver_init()
-    init.test_enter(driver)
-    init.menu_button_click(driver)
-    init.proizvodstvo_button_click(driver)
-    init.regist_of_events_button_click(driver)
-    init.pnsbs_71_button_click(driver)
-    init.continue_without_courier(driver)
-
+def test_10():
+    driver = mod_init.pony_driver_init()
+    mod_init.test_enter(driver)
+    mod_init.menu_button_click(driver)
+    mod_init.proizvodstvo_button_click(driver)
+    mod_init.regist_of_events_button_click(driver)
+    mod_init.pnsbs_71_button_click(driver)
+    mod_init.continue_without_courier(driver)
+    mod_init.nomer_object(driver)
 
 if __name__ == "__main__":
-    test_8()
+    test_10()
