53d9c8267880bbb219407e76217bf883882ba9f4
kva802 <71887839+kva802@users.noreply.github.com>
2020-10-30 15:30:26 +0300

    Update test_case6.py

--- test_case6.py
+++ test_case6.py
@@ -1,15 +1,15 @@
 import mod_init
 
 
-def test_6():
+def test_6(name_of_group):
     driver = mod_init.pony_driver_init()
     mod_init.test_enter(driver)
     mod_init.menu_button_click(driver)
     mod_init.servis_button_click(driver)
     mod_init.uprav_button_click(driver)
     mod_init.groups_button_cick(driver)
-    mod_init.test_creategroup(driver, '1234')
-    mod_init.test_deletegroup(driver, '1234')
+    mod_init.test_creategroup(driver, name_of_group)
+    mod_init.test_deletegroup(driver, name_of_group)
 
 if __name__ == "__main__":
-    test_6()
+    test_6('1234')
