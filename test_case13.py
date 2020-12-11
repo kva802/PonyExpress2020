8480e36be55481f3ad951119440cc1f4e51a37ff
kva802 <71887839+kva802@users.noreply.github.com>
2020-12-11 18:29:17 +0300

    Update test_case13.py

--- test_case13.py
+++ test_case13.py
@@ -1,13 +1,15 @@
 import mod_init
 
 
-def test_10():
+def test_14():
     driver = mod_init.pony_driver_init()
     mod_init.test_enter(driver)
     mod_init.menu_button_click(driver)
     mod_init.proizvodstvo_button_click(driver)
     mod_init.regist_of_events_button_click(driver)
-
+    mod_init.vvk_79_button_click(driver)
+    mod_init.select_and_find_79(driver, '1202')
+    mod_init.number_of_object(driver, '99-9999-9999/999')
 
 if __name__ == "__main__":
-    test_10()
+    test_14()
