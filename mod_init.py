bb29883b080d3de3c024e1d70ff71ed97b6a8c8c
kva802 <71887839+kva802@users.noreply.github.com>
2020-12-11 16:41:32 +0300

    Update mod_init.py

--- mod_init.py
+++ mod_init.py
@@ -28,8 +28,8 @@ def test_enter(driver):
         print('не найдены поля для ввода пользователя и пароля')
         driver.close()
 
-    element_login.send_keys('ext.mgu_education')
-    element_password.send_keys('rg#P5hZm4F')
+    element_login.send_keys(pathes.login)
+    element_password.send_keys(pathes.password)
     enter_button.click()
 
     try:
@@ -87,7 +87,6 @@ def groups_button_cick(driver):
 
         elem = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/section/section[2]/section/div")))
         elem.text == 'Редактирование групп пользователей'
-
     except:
         print('не открылась страница редактирования групп пользователей')
         driver.close()
@@ -176,15 +175,66 @@ def continue_without_courier(driver):
         print('кнопка Продолжить без курьера не найдена, не открылась форма 71. Прибыл на склад (без сортировки)')
         driver.close()
 
-def nomer_object(driver):
+def number_of_object(driver, number_of_object):
     try:
         elem = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/section/section[2]/section/section/div[2]/div[3]/div[2]/div/div/div[3]/form/input")))
-        elem.send_keys('11-1111-1112\n')
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div/span")))
-        time.sleep(5)
+        elem.send_keys(number_of_object)
+        elem.send_keys('\n')
+        #WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div/span")))
+        if elem.get_attribute('style') == 'background-color: rgb(194, 48, 48);':
+            print('err')
+        #time.sleep(5)
     except:
         print('не найдено поле для ввода номера объекта')
         driver.close()
+
+def vvk_79_button_click(driver):
+    try:
+        elem = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,"/html/body/div[3]/div/div/div/div[2]/ul/li[1]/span/div/div/div/div/ul/li[1]/span/div/div[2]/div/div/ul/li[4]/a")))
+        elem.click()
+        time.sleep(2)
+        driver.switch_to_window(driver.window_handles[1])
+        time.sleep(5)
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[3]/div/div[2]/div/div[1]/h4/span"))).text == 'Точка назначения'
+    except:
+        print('не открылась форма Точка назначения')
+        driver.close()
+
+def select_and_find_79(driver, name_of_destination):
+    try:
+        select_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,"/html/body/div[3]/div/div[2]/div/div[2]/form/div[1]/div/button")))
+        select_button.click()
+        find_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[4]/div/div[2]/div/div[2]/div/div[1]/div[2]/div/div/div/input")))
+        find_button.send_keys(name_of_destination)
+    except:
+        print('не открылась форма Выберите точку назначения')
+        driver.close()
+    try:
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[4]/div/div[2]/div/div[2]/div/div[2]/div[1]/table/tbody/tr/td[2]"))).text == name_of_destination
+    except:
+        print('нет точки назначения под таким номером')
+        driver.close()
+
+def add_79(driver):
+    try:
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[4]/div/div[2]/div/div[2]/div/div[2]/div[1]/table/tbody/tr/td[1]/label/span"))).click()
+        add_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[4]/div/div[2]/div/div[2]/div/div[1]/div[1]/button[1]")))
+        add_button.click()
+        dalee_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[3]/div/div[2]/div/div[2]/form/div[2]/button")))
+    except:
+        print('не открылась форма «Точка назначения» с выбранной точкой')
+        driver.close()
+    try:
+        dalee_button.click()
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/section/section[2]/section/section/div[1]/h1"))).text == '79. Включен в консолидацию'
+    except:
+        print('не открылась форма «79. Включен в консолидацию» с номером созданного блока и точкой назначения')
+        driver.close()
+
+
+
+
 if __name__ == "__main__":
     driver = pony_driver_init()
     test_enter(driver)
