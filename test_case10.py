0ce8d55030ea6d447295a070e75b80ac06b6d8c1
kva802 <71887839+kva802@users.noreply.github.com>
2021-01-22 18:33:51 +0300

    Update test_case10.py

--- test_case10.py
+++ test_case10.py
@@ -1,6 +1,6 @@
 import mod_init
 
-def test_11():
+def test_10():
     driver = mod_init.pony_driver_init()
     mod_init.test_enter(driver)
     mod_init.menu_button_click(driver)
@@ -12,4 +12,4 @@ def test_11():
 
 
 if __name__ == "__main__":
-    test_11()
+    test_10()
