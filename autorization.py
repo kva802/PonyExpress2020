0ee323c2d9dc5a1187d487f857506ee4736524b2
kva802 <71887839+kva802@users.noreply.github.com>
2021-02-12 17:27:37 +0300

    Update autorization.py

--- autorization.py
+++ autorization.py
@@ -12,7 +12,7 @@ def autorisation():
             'client_id': 'pegasus-v2',
             'client_secret': 'secret'}
 
-    url = "http://srv-pnew-01-test:1001/auth/connect/token"
+    url = "http://srv-pnew-02-test:1001/auth/connect/token"
 
     r = requests.post(url, data=data, headers=headers)
     answer = json.loads(r.text)
