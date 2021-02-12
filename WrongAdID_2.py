87f3d91f5089669a773dd5e7845202d155cfb6ff
kva802 <71887839+kva802@users.noreply.github.com>
2021-02-12 17:27:50 +0300

    Create WrongAdID_2.py

---
+++ WrongAdID_2.py
@@ -0,0 +1,25 @@
+import json
+import pathes
+import autorization
+import requests
+
+
+def ad_ID_2():
+    try:
+        tok = autorization.autorisation()
+        addressId = '2344f4b3-a7bd-4512-686d-08d8c4533103'
+        url = f'http://geography-backend-edu.pegasus.ponyex.local/api/v1/geography/get-polygon-by-address-id/{addressId}'
+        data = {"addressId": "2344f4b3-a7bd-4512-686d-08d8c4533103"}
+        headers = {'Content-Type': 'application/json',
+                   'Authorization': 'Bearer ' + tok}
+        r = requests.get(url, params=json.dumps(data), headers=headers)
+        answer = json.loads(r.text)
+        if '$_ADDRESS_NOT_FOUND_$' in str(answer['metadata']['message']):
+            print('адреса с таким айди нет')
+        #print(answer)
+    except:
+        print('ошибка')
+
+
+if __name__ == "__main__":
+    ad_ID_2()
