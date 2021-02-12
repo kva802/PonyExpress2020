94fd46dfb8f3581b537eeb6c3f46200f89da89fa
kva802 <71887839+kva802@users.noreply.github.com>
2021-02-12 17:27:42 +0300

    Update pathes.py

--- pathes.py
+++ pathes.py
@@ -1,25 +1,23 @@
 
-
-
 driver_path = r'C:\Users\Арина\Downloads\chromedriver.exe'
 url_path = "http://pegasus-edu.pegasus.ponyex.local/"
 
 login = 'ext.mgu2_education'
 password = 'rg#P5hZm4F'
 
-urls =[
-     'http://configurations-backend-edu.pegasus.ponyex.local/api/manual/index.html',
-     'http://couriers-backend-edu.pegasus.ponyex.local/api/manual/index.html',
-     'http://delivery-edu.pegasus.ponyex.local/api/manual/index.html',
-     'http://delivery-backend-edu.pegasus.ponyex.local/api/manual/index.html',
-     'http://enumerations-backend-edu.pegasus.ponyex.local/api/manual/index.html',
-     'http://events-edu.pegasus.ponyex.local/api/manual/index.html',
-     'http://events-backend-edu.pegasus.ponyex.local/api/manual/index.html',
-     'http://geography-edu.pegasus.ponyex.local/api/manual/index.html',
-     'http://geography-backend-edu.pegasus.ponyex.local/api/manual/index.html',
-     'http://localizations-backend-edu.pegasus.ponyex.local/api/manual/index.html',
-     'http://organization-backend-edu.pegasus.ponyex.local/api/manual/index.html',
-     'http://warehouses-edu.pegasus.ponyex.local/api/manual/index.html',
-     'http://warehouses-backend-edu.pegasus.ponyex.local/api/manual/index.html',
-     'http://waybills-backend-edu.pegasus.ponyex.local/api/manual/index.html'
-    ]
+urls = [
+    'http://configurations-backend-edu.pegasus.ponyex.local/api/manual/index.html',
+    'http://couriers-backend-edu.pegasus.ponyex.local/api/manual/index.html',
+    'http://delivery-edu.pegasus.ponyex.local/api/manual/index.html',
+    'http://delivery-backend-edu.pegasus.ponyex.local/api/manual/index.html',
+    'http://enumerations-backend-edu.pegasus.ponyex.local/api/manual/index.html',
+    'http://events-edu.pegasus.ponyex.local/api/manual/index.html',
+    'http://events-backend-edu.pegasus.ponyex.local/api/manual/index.html',
+    'http://geography-edu.pegasus.ponyex.local/api/manual/index.html',
+    'http://geography-backend-edu.pegasus.ponyex.local/api/manual/index.html',
+    'http://localizations-backend-edu.pegasus.ponyex.local/api/manual/index.html',
+    'http://organization-backend-edu.pegasus.ponyex.local/api/manual/index.html',
+    'http://warehouses-edu.pegasus.ponyex.local/api/manual/index.html',
+    'http://warehouses-backend-edu.pegasus.ponyex.local/api/manual/index.html',
+    'http://waybills-backend-edu.pegasus.ponyex.local/api/manual/index.html'
+]
