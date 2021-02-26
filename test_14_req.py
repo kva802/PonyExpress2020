470a9bc974bc98dae75739230eec6cb65bdb21f5
kva802 <71887839+kva802@users.noreply.github.com>
2021-02-26 09:36:33 +0300

    Update test_14_req.py

--- test_14_req.py
+++ test_14_req.py
@@ -22,18 +22,24 @@ def test_request_14():
 
         r = event_79_request.included_in_consolidation(token, destinationPointId)
 
+        assert r == 'ERROR' or r['result'] is None, 'Не смог создать блок событий 79 - включён в консолидацию, ' \
+                                                    'Тест 14 не пройден '
+        '''
         if r == 'ERROR' or r['result'] is None:
             print('Не смог создать блок событий 79 - включён в консолидацию')
             print('Тест 14 не пройден')
             return
-
+        '''
         destinationPoint = r['result']['destinationPoint']
 
+        assert destinationPoint['code'] != '1202', 'Попал в другой блок, Тест 14 не пройден'
+        '''
         if destinationPoint['code'] != '1202':
             print('Попал в другой блок')
             print('Тест 14 не пройден')
             return
-
+            
+        '''
         print('Блок успешно создан')
 
         id_ = r['result']['id']
@@ -42,10 +48,19 @@ def test_request_14():
 
         r = event_79_request.add_object(token, id_, number)
 
+        assert r == 'ERROR', 'Не смог вбить номер места накладной, Тест 14 не пройден'
+        '''
         if r == 'ERROR':
             print('Не смог вбить номер места накладной')
             print('Тест 14 не пройден')
             return
+        '''
+        assert r['metadata']['message'] == "$_PLACE_IS_OUT_OF_RANGE_$", 'Система вернула ошибку «Отсутствует ' \
+                                                                        'введенное место накладной», Тест 14 пройден '
+
+        assert r['metadata']['message'] != "$_PLACE_IS_OUT_OF_RANGE_$", 'Система нашла номер места накладной: ' + number
+
+        '''
 
         if r['metadata']['message'] == "$_PLACE_IS_OUT_OF_RANGE_$":
             print('Система вернула ошибку «Отсутствует введенное место накладной»')
@@ -55,6 +70,7 @@ def test_request_14():
             print('Система нашла номер места накладной: ' + number)
             print('Тест 14 не пройден')
             return
+        '''
 
 
 if __name__ == "__main__":
